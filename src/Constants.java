d6b700f4922523f35da5a4e53fd62a5e3d96faac
Bhavit Jain <bhavitjain902@gmail.com>
2024-10-31 03:53:02 +0530

    refactored Main.java

--- src/Constants.java
+++ src/Constants.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.*;
+
 public class Constants {
     public static final String ADD = "add";
     public static final String UPDATE = "update";
@@ -13,4 +15,16 @@ public class Constants {
     public static final String INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of arguments provided";
     public static final String INVALID_AN_TASK_ID = "is invalid an task id. task id must be a integer";
     public static final String INVALID_ARGUMENT = "is a invalid argument";
+
+    public static final List<String> TASK_LIST = List.of(ADD, UPDATE, DELETE, MARK_IN_PROGRESS, MARK_DONE, LIST);
+    public static final List<String> STATUS_LIST = List.of(TODO, IN_PROGRESS, DONE);
+    public static final Map<String, Set<Integer>> TASK_ARGUMENTS_LIST_MAP = new HashMap<>() {{
+        put(ADD, new HashSet<>(List.of(2)));
+        put(UPDATE, new HashSet<>(List.of(3)));
+        put(DELETE, new HashSet<>(List.of(2)));
+        put(MARK_IN_PROGRESS, new HashSet<>(List.of(2)));
+        put(MARK_DONE, new HashSet<>(List.of(2)));
+        put(LIST, new HashSet<>(List.of(1, 2)));
+    }};
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 package src;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static List<String> taskList = List.of(Constants.ADD, Constants.UPDATE, Constants.DELETE, Constants.MARK_IN_PROGRESS, Constants.MARK_DONE, Constants.LIST);
-    static List<String> statusList = List.of(Constants.TODO, Constants.IN_PROGRESS, Constants.DONE);
-
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         while (true) {
@@ -22,63 +17,47 @@ public class Main {
     }
 
     private static void performOperation(String[] args) {
-        if (args.length <= 1 || args.length > 3) {
-            System.out.println(Constants.INVALID_NUMBER_OF_ARGUMENTS);
+        String task = args[0].toLowerCase();
+
+        if (!Constants.TASK_LIST.contains(task)) {
+            System.out.println(task + Constants.INVALID_ARGUMENT);
             return;
         }
 
-        String task = args[0].toLowerCase();
-
-        if (!taskList.contains(task)) {
-            System.out.println(task + Constants.INVALID_ARGUMENT);
+        if (!Constants.TASK_ARGUMENTS_LIST_MAP.get(task).contains(args.length)) {
+            System.out.println(Constants.INVALID_NUMBER_OF_ARGUMENTS);
             return;
         }
 
         try {
             switch (task) {
                 case Constants.LIST -> {
-                    if (args.length != 2) {
-                        System.out.println(Constants.INVALID_NUMBER_OF_ARGUMENTS);
+                    if (args.length == 1){
+                        listAllTasks();
                         return;
                     }
 
                     String status = args[1];
-                    if (!statusList.contains(status)) {
+                    if (!Constants.STATUS_LIST.contains(status)) {
                         System.out.println(status + Constants.INVALID_ARGUMENT);
                         return;
                     }
                     listTaskByStatus(status);
                 }
                 case Constants.ADD -> {
-                    if (args.length != 2) {
-                        System.out.println(Constants.INVALID_NUMBER_OF_ARGUMENTS);
-                        return;
-                    }
                     String description = args[1];
                     addTask(description);
                 }
                 case Constants.UPDATE -> {
-                    if (args.length != 3) {
-                        System.out.println(Constants.INVALID_NUMBER_OF_ARGUMENTS);
-                        return;
-                    }
                     int taskId = Integer.parseInt(args[1]);
                     String description = args[2];
                     updateTask(taskId, description);
                 }
                 case Constants.DELETE -> {
-                    if (args.length != 2) {
-                        System.out.println(Constants.INVALID_NUMBER_OF_ARGUMENTS);
-                        return;
-                    }
                     int taskId = Integer.parseInt(args[1]);
                     deleteTask(taskId);
                 }
                 default -> {
-                    if (args.length != 2) {
-                        System.out.println(Constants.INVALID_NUMBER_OF_ARGUMENTS);
-                        return;
-                    }
                     int taskId = Integer.parseInt(args[1]);
                     changeTaskStatus(taskId, task);
                 }
@@ -86,6 +65,9 @@ public class Main {
         } catch (NumberFormatException ignore) {
             System.out.println(args[1] + Constants.INVALID_AN_TASK_ID);
         }
+    }
+
+    private static void listAllTasks() {
     }
 
 
